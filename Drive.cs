ab45ed7ef86d9fd10af77a9f1c646448a321918b
SeokGyuChoi <37360803+SeokGyuChoi@users.noreply.github.com>
2019-04-21 01:44:34 +0900

    Drive

---
+++ Drive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    private float inputHorizontal;
+    private float inputVertical;
+
+    private string horizontalAxis = "Horizontal";
+    private string verticalAxis = "Vertical";
+
+    void Update()
+    {
+        transform.Translate(0, 0, 1 * Time.deltaTime * 5f);
+
+        inputHorizontal = SimpleInput.GetAxis(horizontalAxis);
+        inputVertical = SimpleInput.GetAxis(verticalAxis);
+
+        transform.Rotate(0, inputHorizontal * 5f, 0f, Space.Self);
+    }
+}
