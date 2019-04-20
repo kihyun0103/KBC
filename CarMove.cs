a835b9b187c86376b8327411ea5efaacf73f98dd
SeokGyuChoi <37360803+SeokGyuChoi@users.noreply.github.com>
2019-04-21 01:45:44 +0900

    CarMove

---
+++ CarMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    int speed = 10;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        moveObject();
+    }
+
+    void moveObject()
+
+    {
+
+        float keyHorizontal = Input.GetAxis("Horizontal");
+
+        float keyVertical = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.right * speed * Time.smoothDeltaTime * keyHorizontal, Space.World);
+
+        transform.Translate(Vector3.forward * speed * Time.smoothDeltaTime * keyVertical, Space.World);
+
+    }
+
+    
+}
