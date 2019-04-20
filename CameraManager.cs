33addbf1176fa73a022c54a5d1f402c1e8480a31
SeokGyuChoi <37360803+SeokGyuChoi@users.noreply.github.com>
2019-04-21 01:46:04 +0900

    CameraManager

---
+++ CameraManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera firstCamera;
+    public Camera secondCamera;
+
+    public void secondView()
+    {
+        firstCamera.enabled = false;
+        secondCamera.enabled = true;
+    }
+
+    public void firstView()
+    {
+        firstCamera.enabled = true;
+        secondCamera.enabled = false;
+    }
+
+    public void Update()
+    {
+        if(Input.GetKey("1"))
+        {
+            secondView();
+        }
+
+        if(Input.GetKey("2"))
+        {
+            firstView();
+        }
+    }
+}
