2070eed73645f9714790fcd720845b02d717dd51
SeokGyuChoi <37360803+SeokGyuChoi@users.noreply.github.com>
2019-05-06 19:54:16 +0900

    CameraLookAt

---
+++ Billboard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+	
+	void Update () {
+        transform.LookAt(FindObjectOfType<Camera>().transform);    //LookAt 어떤 위치를 주면 그 방향을 계속 보고있음
+                                                                   //현재 활성화되고있는 카메라
+    }
+}
