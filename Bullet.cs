e2d002f9b65ef45b20d209858a05255df8ac3683
SeokGyuChoi <37360803+SeokGyuChoi@users.noreply.github.com>
2019-05-06 19:53:15 +0900

    Bullet

---
+++ Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Bullet : MonoBehaviour {
+    private void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * 6.0f;
+        Destroy(gameObject, 2.0f);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+
+        if(health != null)
+        {
+            health.TakeDamage(10);
+        }
+
+        Destroy(gameObject);
+    }
+}
