3c9ff88ed3af16ff6dcb76b0820c9891ab7c9d85
SeokGyuChoi <37360803+SeokGyuChoi@users.noreply.github.com>
2019-06-11 23:29:15 +0900

    carcontrolactive(2)

---
+++ CarControlActive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CarControlActive : MonoBehaviour
+{
+    public GameObject CarControl;
+    public GameObject Dreamcar01;
+
+    void Start()
+    {
+        CarControl.GetComponent<CarController>().enabled = true;
+        Dreamcar01.GetComponent<UnityStandardAssets.Vehicles.Car.CarAIControl>().enabled = true;
+    }
+}
